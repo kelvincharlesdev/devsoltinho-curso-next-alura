a93baa44c12d194f1d5fcf9a9fb2a111f0252fdd
Kelvin Charles <kelvinc1291@hotmail.com>
2024-09-06 15:46:28 -0300

    Adjusts

--- pages/_app.js
+++ pages/_app.js
@@ -6,7 +6,7 @@ export default function MyApp({ Component, pageProps }) {
         <>
             <Head>
                 <link rel="preconnect" href="https://fonts.googleapis.com"/>
-                <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+                <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin={true.toString} />
                 <link href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300..800;1,300..800&display=swap" rel="stylesheet"/>
             </Head>
             <GlobalStyle/>
